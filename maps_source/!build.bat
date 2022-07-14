a9d9ade4fa6a9d83908314adc2c9819fb4ea880d
Gabriel <9045568+BLACKM3SA@users.noreply.github.com>
2022-07-14 01:57:38 -0500

    Update !build.bat

--- maps_source/!build.bat
+++ maps_source/!build.bat
@@ -2,15 +2,17 @@
 color 0a
 title Build
 
-START /wait !domap.bat a1c2p2_downtown_2
-START /wait !domap.bat a1c3p1_canal
+START /wait !domap.bat a3c3p1_blackmesaeast
+START /wait !domap.bat a3c1p2_jump
 START /wait !domap.bat a1c5p4_ship
-START /wait !domap a3c1p2_jump
-START /wait !domap a3c2p3_ci17-3
-START /wait !domap a3c3p1_blackmesaeast
-START /wait !domap a3c5p2_highway
-START /wait !domap a3c8p2_outpost
-START /wait !domap credits
+START /wait !domap.bat a2c1p1_alaska
+
+
+
+
+
+
+REM START /wait !domap_minimal_vrad.bat a3c8p2_outpost
 
 ECHO DONE
 PAUSE
