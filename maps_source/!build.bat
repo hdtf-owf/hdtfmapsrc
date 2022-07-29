c40868e8bc79e2b62e29f973dd372809fa5859af
Gabriel <9045568+BLACKM3SA@users.noreply.github.com>
2022-07-29 16:40:46 -0500

    replaced m16 with the carbine on npcs

--- maps_source/!build.bat
+++ maps_source/!build.bat
@@ -2,11 +2,18 @@
 color 0a
 title Build
 
-START /wait !domap.bat a3c4p1_cave
+START /wait !domap.bat a1c2p1_downtown_1
+START /wait !domap.bat a1c2p2_downtown_2
+START /wait !domap.bat a1c3p2_urban
+START /wait !domap.bat a1c3p4_metro_2
+START /wait !domap.bat a1c3p5_metro_3
+START /wait !domap.bat a1c3p6_station
+START /wait !domap.bat a1c4p3_outlands_1
+START /wait !domap.bat a1c5p2_pier_2
 
 
-START /wait !domap_minimal_vrad.bat a1c1p1_hospital_1
-START /wait !domap_minimal_vrad.bat a3c8p2_outpost
+REM START /wait !domap_minimal_vrad.bat a1c1p1_hospital_1
+REM START /wait !domap_minimal_vrad.bat a3c8p2_outpost
 
 ECHO DONE
 PAUSE
