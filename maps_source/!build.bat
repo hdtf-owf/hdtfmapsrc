3f33a884aaf76b9a4d432fe1087bd7d70e44600a
Terrors <79553151+Terrrors@users.noreply.github.com>
2023-03-15 00:24:35 +0800

    updated build.bat

--- maps_source/!build.bat
+++ maps_source/!build.bat
@@ -2,13 +2,6 @@
 color 0a
 title Build
 
-START /wait !domap.bat a2c1p1_alaska
-START /wait !domap.bat a3c1p1_ship
-START /wait !domap.bat a2c2p2_factory
-START /wait !domap.bat a1c3p3_metro_1
-START /wait !domap.bat a1c4p2_mesa
-START /wait !domap.bat a1c4p4_factory
-START /wait !domap.bat a3c7p2_prison_2
 START /wait !domap.bat a0c0p1_blackmesa
 START /wait !domap.bat a1c1p1_hospital_1
 START /wait !domap.bat a1c1p2_hospital_2
@@ -30,11 +23,6 @@ START /wait !domap.bat a3c7p1_prison_1
 START /wait !domap.bat a3c8p1_forest
 
 
-
-START /wait !domap_minimal_vrad.bat a1c5p1_pier_1
-START /wait !domap_minimal_vrad.bat a3c8p2_outpost
-START /wait !domap_minimal_vrad.bat a3c2p1_ci17-1
-START /wait !domap_minimal_vrad.bat a3c2p2_ci17-2
 START /wait !domap_minimal_vrad.bat a3c2p3_ci17-3
 
 ECHO DONE
