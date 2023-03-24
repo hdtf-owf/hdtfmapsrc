009e0aab5a85c87c33fedab3adcef440ff5babff
Kerim Čamdžič <hl2.for.you@gmail.com>
2023-03-24 02:50:23 +0100

    made the map compile script be abel to take in a param from the txt

--- maps_source/!build_maps_from_file.bat
+++ maps_source/!build_maps_from_file.bat
@@ -2,7 +2,24 @@
 color 0a
 title Build
 
-for /F "tokens=*" %%A in (!maps_to_compile.txt) do START /wait !domap.bat %%A
+for /F "tokens=1,2* delims= " %%A in (!maps_to_compile.txt) do (
+	
+	SET FULL = full
+	SET MINIMAL = minimal
+	
+	if %%B==minimal (
+		ECHO DOING MINIMAL COMPILE
+		START /wait !domap_minimal_vrad.bat %%A
+	) else (
+		START /wait !domap.bat %%A
+		ECHO DOING FULL COMPILE
+	)
+	
+	if %%B==full (
+		START /wait !domap.bat %%A
+	)
+	
+)
 
 ECHO DONE
 PAUSE
