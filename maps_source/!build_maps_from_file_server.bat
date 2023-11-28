8a95b8a1082c84112d2debfc186ef022fffc613c
Gabriel <9045568+BLACKM3SA@users.noreply.github.com>
2023-11-28 02:40:22 -0600

    compile times + bat update

--- maps_source/!build_maps_from_file_server.bat
+++ maps_source/!build_maps_from_file_server.bat
@@ -2,7 +2,7 @@
 color 0a
 title Build
 
-for /F "tokens=1,2* delims= " %%A in (maps_to_compile.txt) do (
+for /F "tokens=1,2* delims= " %%A in (!maps_to_compile.txt) do (
     echo Processing map: %%A
     START /wait !domap_server.bat %%A
 )
