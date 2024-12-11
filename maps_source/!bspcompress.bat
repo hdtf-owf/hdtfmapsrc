ffc141e3d43192a462217c5fdae2e4971717f7b3
Terrrors <aar0n22@outlook.com>
2024-12-11 17:17:39 +0800

    Most up to date bspcompress

--- maps_source/!bspcompress.bat
+++ maps_source/!bspcompress.bat
@@ -1,14 +1,39 @@
 @echo off
 color 0a
-title %1
+title %1 - Copyright (c) 2023 M3SA.
+setlocal
 
-SET GAME_DIR=G:\hdtf_contentbuilder\steamworks_sdk\tools\ContentBuilder\content\generic\
-SET VPROJECT=%GAME_DIR%\hdtf
+cd /d "%~dp0"
+cd /d ..
+cd /d ..
 
-"%GAME_DIR%\bin\bspzip.exe" -repack -compress %1
+SET GAME_DIR=%cd%
+SET BIN_DIR=%GAME_DIR%\Bin
+SET MAP_DIR=%GAME_DIR%\HDtF\maps
+SET VPROJECT=%GAME_DIR%\HDtF
 
-@pause
+REM Check if a file was dropped onto the batch file
+if "%~1.bsp"=="" (
+  echo.
+  echo GAME_DIR:
+  echo %GAME_DIR%
+  echo.
+  echo BIN:
+  echo %BIN_DIR%
+  echo.
+  echo MAPS:
+  echo %MAP_DIR%
+  echo.
+  echo VPROJECT:
+  echo %VPROJECT%
+  echo.
+  echo CURRENT DIRECTORY:
+  echo %cd%
+  exit /b 1
+)
 
-REM "..\..\bin\bspzip" -game HDtF -repack -compress %1
-REM "..\..\bin\bspzip" -repack -compress %1
-REM @pause
+echo File "%~1" was dropped onto the batch file.
+
+"%BIN_DIR%\bspzip.exe" -repack -compress "%MAP_DIR%\%~1.bsp"
+
+exit
